7c3cf8b631fac36d45f92c31c5f9a6bae1d740b6
mikey <milehimikey@gmail.com>
2025-11-18 20:55:06 -0700

    more stuff

--- src/main/kotlin/com/axon/jpa/connector/eventstream/ReadOnlyEventStream.kt
+++ src/main/kotlin/com/axon/jpa/connector/eventstream/ReadOnlyEventStream.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024. Axon JPA Connector
+ * Copyright (c) 2024. MileHiMikey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
